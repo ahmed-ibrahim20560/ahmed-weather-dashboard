ee1292537a1ee1ee5e27f82520cd14039a3a9e0d
ahmed-ibrahim20560 <ahmedibrahim20560@gmail.com>
2024-01-16 21:16:51 +0000

    unfortunately i was working in the class repo so i ended up having to copy from there. i will try explain as best as possible. i created a function which has multiple fetch calls for both the current weather and for the forecasted weather. the first fetch receives data from the geo in order for me to get the city name as well as the latitude and the longitude for a specific city which will both be needed for the forecast. the second fetch gets data for the forecast and i set a variable to allow me to start from the data.list array and it also runs the next function in order to set the parameter with data that i would need to pass through it in order to use that data in that function as well. i then made a for loop which appends the different created elements with the required attrbutes for the future forecast and had hardcoded the html for the current weather whilst adding attributes to them from the javascript

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,75 @@
 const key = "d8edc27e021c7cfd8f0799f88d400613";
 
+function fetchWeather(search) {
+  let queryUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${search}&limit=5&appid=${key}`;
 
+  fetch(queryUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      let latitude = data[0].lat;
+      let longitude = data[0].lon;
+      let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${key}`;
+
+      const h3El = $('#card-title').text(`${data[0].name} (${dayjs().format('MMMM D, YYYY')})`);
+
+      fetch(forecastURL)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          const currentWeather = data.list
+          displayCurrentWeather(data.list[0]);
+
+          const fiveDayForecast = currentWeather.filter(function(data) {
+            return data.dt_txt.includes('12:00:00')
+          }
+          )
+          console.log(`five day forecast: ${fiveDayForecast}`)
+
+
+          const iconCode = currentWeather[0].weather[0].icon;
+          const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+          $('#forecast').empty();
+
+
+          for (let i = 0; i < fiveDayForecast.length; i++) {
+            const day = fiveDayForecast[i];
+            
+            const cardCol = $('<div>').attr('class', 'col-md')
+            const forecastCard = $('<div>').attr('class', 'card')
+            const forecastBody = $('<div>').attr('class', 'card-body')
+            const forecastCardTitle = $('<h5>').attr('class', 'card-title').text(dayjs(day.dt_txt).format('MM-DD-YYYY'))
+            const forecastTemp = $('<p>').text(`Temp: ${day.main.temp} C`)
+            const forecastWind = $('<p>').text(`Wind: ${day.wind.speed} kph`)
+            const forecastHumidity = $('<p>').text(`Humidity: ${day.main.humidity} %`)
+            const forecastIcon = $('<img>').attr('src', iconUrl)
+
+            $('#forecast').append(cardCol)
+            cardCol.append(forecastCard)
+            forecastCard.append(forecastBody)
+            forecastBody.append(forecastCardTitle, forecastIcon, forecastTemp, forecastWind, forecastHumidity)
+            
+          }
+        });
+    });
+}
+
+
+function displayCurrentWeather(currentWeather) {
+  $('#icon').empty()
+  const iconCode = currentWeather.weather[0].icon;
+  const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+  console.log(currentWeather.weather[0].icon)
+  const icon = $('<img>').attr('src', iconUrl)
+  const temp = $('#temp').text(`Temperature: ${currentWeather.main.temp} C`);
+  const wind = $('#wind').text(`Wind: ${currentWeather.wind.speed} kph`);
+  const humidity = $('#humidity').text(`Humidity: ${currentWeather.main.humidity} %`);
+  $('#icon').append(icon)
+  
+}
 
 $('#search-button').on('click', function (e) {
   e.preventDefault();
